4c4758c7b4f24a20c82034e192502a16b0b52fdb
Kheagen Haskins <slinky.haskins@gmail.com>
2024-08-13 23:17:59 +0200

    Skeleton Implementations Uploaded  - MainInterface.java  - SymbolCluster.java

---
+++ src/main/java/com/codinwithslinky/terminaltakedown/SymbolCluster.java
@@ -0,0 +1,25 @@
+package com.codinwithslinky.terminaltakedown;
+
+/**
+ *
+ * @author Kheagen
+ */
+public class SymbolCluster extends AbstractCluster {
+
+    // ------------------------------ Fields -------------------------------- //
+
+    // --------------------------- Constructors ----------------------------- //
+
+    // ------------------------------ Getters ------------------------------- //
+
+    // ------------------------------ Setters ------------------------------- //
+
+    // ---------------------------- API Methods ----------------------------- //
+    @Override
+    public boolean validate() {
+        return true;
+    }
+    
+    // -------------------------- Helper Methods ---------------------------- //
+
+}
