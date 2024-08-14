217d04fdebc0755925dc1ea142b159dfe5d2b140
Kheagen Haskins <slinky.haskins@gmail.com>
2024-08-14 23:19:30 +0200

    Documented Class

--- src/main/java/com/codinwithslinky/terminaltakedown/cell/LetterCluster.java
+++ src/main/java/com/codinwithslinky/terminaltakedown/cell/LetterCluster.java
@@ -1,23 +1,50 @@
 package com.codinwithslinky.terminaltakedown.cell;
 
 /**
+ * A specialized implementation of {@code AbstractCluster} that groups
+ * {@code LetterCell}s. This class ensures that only cells containing letter
+ * characters are added to the cluster.
+ * <p>
+ * The {@code LetterCluster} class enforces that only instances of
+ * {@code LetterCell} can be added, and validates that the content of each cell
+ * is a letter.
+ * </p>
  *
  * @author Kheagen Haskins
  */
 public class LetterCluster extends AbstractCluster {
 
     // ---------------------------- API Methods ----------------------------- //
+    /**
+     * Adds a {@code Cell} to the cluster, ensuring it is an instance of
+     * {@code LetterCell}.
+     *
+     * @param cell The {@code Cell} to be added to the cluster.
+     * @return {@code true} if the cell was added successfully, {@code false}
+     * otherwise.
+     * @throws IllegalArgumentException If the given cell is not an instance of
+     * {@code LetterCell}.
+     */
     @Override
     public boolean addCell(Cell cell) {
+        if (!(cell instanceof LetterCell)) {
+            throw new IllegalArgumentException("Cannot add a Cell of type " + cell.getClass().getName() + " to a LetterCluster");
+        }
 
         return super.addCell(cell);
     }
 
     /**
-     * Ensures that this cluster only has letters inside. However, failing this method
-     * should be guarded by the {@link #addCell(Cell cell)} method.
+     * Validates that all cells in this cluster contain letter characters.
+     * <p>
+     * This method checks each cell in the cluster to ensure its content is a
+     * letter. Normally, this validation should pass, as the
+     * {@link #addCell(Cell cell)} method is designed to prevent non-letter
+     * cells from being added.
+     * </p>
      *
-     * @return
+     * @return {@code true} if all cells in the cluster contain letters,
+     * {@code false} otherwise.
      */
     @Override
     public boolean validate() {
