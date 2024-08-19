2e76096d9a48f014eaa17e90403ecc31a33d3593
Kheagen Haskins <slinky.haskins@gmail.com>
2024-08-19 19:01:09 +0200

    New Utility Package Introduced - Planned to hold various utility classes as the need arises. - Added StringUtil.java as a central home to any needed string functions

---
+++ src/main/java/com/codinwithslinky/terminaltakedown/util/StringUtil.java
@@ -0,0 +1,33 @@
+package com.codinwithslinky.terminaltakedown.util;
+
+/**
+ * 
+ * @author Kheagen Haskins
+ */
+public class StringUtil {
+
+    // ---------------------------- API Methods ----------------------------- //
+    /**
+     * Calculates and returns the total number of characters across all words in
+     * the word list.
+     * <p>
+     * This method iterates through each word in the {@code wordList} and sums
+     * up their lengths to determine the total number of characters. This can be
+     * useful for operations where the cumulative length of the words is
+     * required, such as when determining the size of a jumbled string.
+     * </p>
+     *
+     * @return the total number of characters in all words within this
+     * {@code StaticWordSet}.
+     */
+    public static int getTotalCharacters(String[] wordList) {
+        int sum = 0;
+        for (int i = 0; i < wordList.length; i++) {
+            sum += wordList[i].length();
+        }
+        return sum;
+    }
+    
+    // -------------------------- Helper Methods ---------------------------- //
+
+}
