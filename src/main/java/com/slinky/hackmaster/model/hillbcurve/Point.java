44e5c91d9837677135949457229897d37aedc759
Kheagen Haskins <slinky.haskins@gmail.com>
2024-08-25 19:21:02 +0200

    feat: Adding a Hillbert Curve Implementation; fundamental components added
    - Added the Point.java record to represent a point in 2D space.
    - Added the Rotation.java enum to represent a clockwise or counterclockwise rotation
    - Added the Tile.java class that represents a first-order Hillbert Curve

---
+++ src/main/java/com/slinky/hackmaster/model/hillbcurve/Point.java
@@ -0,0 +1,25 @@
+package com.slinky.hackmaster.model.hillbcurve;
+
+/**
+ * Represents a single point in 2D space.
+ * <p>
+ * This record encapsulates a point in a two-dimensional Cartesian coordinate
+ * system. The point is defined by its x (horizontal) and y (vertical)
+ * coordinates, which are both integers.
+ * </p>
+ *
+ * <p>
+ * Example usage:
+ * <pre>
+ * Point point = new Point(3, 5);
+ * int xCoord = point.x(); // returns 3
+ * int yCoord = point.y(); // returns 5
+ * </pre>
+ * </p>
+ *
+ * @param x the x-coordinate of the point
+ * @param y the y-coordinate of the point
+ *
+ * @author Kheagen Haskins
+ */
+public record Point(int x, int y) {}
